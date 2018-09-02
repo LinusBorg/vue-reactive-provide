cf0c43a6fbd81e7a114e4647d7d75d267cb21055
Thorsten <t.luenborg@googlemail.com>
2018-09-02 16:56:04 +0200

    removing boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'app',
+  components: {},
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
