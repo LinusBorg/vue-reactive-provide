7d21ad54d0d0a8f1702b2cb8f6d8366bd307127c
Thorsten <t.luenborg@googlemail.com>
2018-10-21 16:42:04 +0200

    fix babel config for smaller build

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  presets: ['@vue/app'],
-  // presets: [
-  //   !process.env.VUE_APP_ROLL
-  //     ? '@vue/app'
-  //     : [
-  //         '@babel/env',
-  //         {
-  //           useBuiltIns: false,
-  //           modules: false,
-  //           targets: '> 3 %, Safari >= 11, not dead',
-  //         },
-  //       ],
-  // ],
+  //presets: ['@vue/app'],
+  presets: [
+    process.env.NODE_ENV !== 'production'
+      ? '@vue/app'
+      : [
+          '@babel/env',
+          {
+            useBuiltIns: false,
+            modules: false,
+            targets: '> 1 %, not dead, last 2 versions, no ie <= 11',
+          },
+        ],
+  ],
 }
